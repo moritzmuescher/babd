6511d300ce10d20585475db907328cc8aea7395a
Moritz Müscher <m.muscher@student.fontys.nl>
2025-07-29 00:07:32 +0200

    move LNURL higher on mobile

--- components/social-link.tsx
+++ components/social-link.tsx
@@ -4,7 +4,7 @@ import { Card } from "@/components/ui/card"
 
 export function SocialLink() {
   return (
-    <div className="absolute left-4 bottom-32 md:top-1/2 md:bottom-auto md:transform md:-translate-y-1/2 z-10">
+    <div className="absolute left-4 bottom-44 md:top-1/2 md:bottom-auto md:transform md:-translate-y-1/2 z-10">
       <Card className="bg-black/50 border-orange-500/25 backdrop-blur-sm">
         <a
           href="https://x.com/babdcs"
