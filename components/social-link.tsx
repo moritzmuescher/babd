796b4d8e8494d4ddc2bab0b06bc7691614f21ff2
Moritz Müscher <m.muscher@student.fontys.nl>
2025-07-28 23:53:39 +0200

    fix LN URL button location

--- components/social-link.tsx
+++ components/social-link.tsx
@@ -4,7 +4,7 @@ import { Card } from "@/components/ui/card"
 
 export function SocialLink() {
   return (
-    <div className="absolute left-4 bottom-20 z-10">
+    <div className="absolute left-4 top-1/2 transform -translate-y-1/2 md:top-1/2 md:transform md:-translate-y-1/2 bottom-20 md:bottom-auto z-10">
       <Card className="bg-black/50 border-orange-500/25 backdrop-blur-sm">
         <a
           href="https://x.com/babdcs"
