320e6a8eb4ef7e6d6364e2f28a292a6761d9bc2e
Moritz Müscher <m.muscher@student.fontys.nl>
2025-08-19 13:31:12 +0200

    replace LN QR

--- components/donation-qr.tsx
+++ components/donation-qr.tsx
@@ -23,8 +23,8 @@ export function DonationQR() {
     <div className="absolute right-4 bottom-44 md:top-1/2 md:bottom-auto md:transform md:-translate-y-1/2 z-10">
       <Card className="bg-black/50 border-orange-500/25 backdrop-blur-sm p-2 md:p-4 text-center">
         <img
-          src="/images/ln_qr.jpeg"
-          alt="Wallet of Satoshi Lightning Network QR Code"
+          src="/images/LNURL2png.png"
+          alt="Lightning QR Code"
           className="w-48 h-48 rounded-lg mb-3 cursor-pointer hover:opacity-80 transition-opacity hidden md:block"
           onClick={handleCopy}
         />
